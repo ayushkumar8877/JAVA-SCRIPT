c5540aedcf41f510f78f5e4cfd699a906c148c85
Ayush Kumar <168068991+ayushkumar8877@users.noreply.github.com>
2025-03-16 15:24:27 +0530

    Add files via upload

---
+++ 01_BASICS/conversionOperation_03.js
@@ -0,0 +1,33 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score ) 
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+ 
+
+ let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "Ayush " => true
+// "" => false
+
+
+let somenumber = 101;
+
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
